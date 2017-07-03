c834d691474a7b887b8aa95498386044f38744eb
Travis <tlgreg86@gmail.com>
2017-07-03 13:27:08 -0600

    Add svg icons for buttons and build out card and card list

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Card from '../Card/Card'
 import './CardList.css';
 
 const CardList = () => {
   return (
     <div className='card-list'>
-
+      <Card name={'Luke Skywalker'}
+            homeworld={'Tattoine'}
+            species={'Human'}
+            population={'150000'}/>
+      <Card name={'Tattoine'}
+            terrain={'rough'}
+            population={'150000'}
+            climate={'dry'}
+            residents={'Luke Skywalker'}/>
+      <Card name={'AT-AT'} 
+            model={'F-150'}
+            class={'pickup'}
+            numPassengers={'8'}/>
     </div>
   );
 }
