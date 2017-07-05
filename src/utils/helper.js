6ccdcb52008f8f56b136f8338053faf0eddee065
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-05 17:51:04 -0600

    Add WIP for nested fetch calls

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,14 +1,45 @@
 const fetchCall = () => {
   let p1 = fetch('http://swapi.co/api/people/')
-      .then(response => response.json())
+    .then(response => response.json())
 
   return Promise.all([p1])
-    .then(values => {
-      return scrubData(values);
+    .then(value => {
+      let promisesArray = value[0].results.map( person => 
+        fetch(person.homeworld)
+        .then(response => response.json())
+        // .then(response => return response)
+      )
+      
+      console.log(promisesArray);
+      return Promise.all(promisesArray).then((data) => console.log(data));
+      
+      
+      
+      
+      // return scrubData(value);
     })
 }
 
+// fetch('http://localhost:3001/api/frontend-staff')
+// .then((res) => res.json())
+// .then((info) => {
+//   const promises = info.bio.map((i) => fetch(i.info).then((res) => res.json()))
+// 
+//   //Now we're going to wait for all the promises to resolve
+//   return Promise.all(promises).then((members) => members.map((person, i) => Object.assign(info.bio[i], person)))
+// 
+//   // and assign them to the original object. To add the additional info.
+// 
+//   // we returned the promise.all to the original fetch promise so we can:
+//   .then((people) => this.setState({ staff: people }))
+//   .catch((error) => console.log(error))
+// })
+
 const scrubData = (data) => {
+  // [ { results: [name: , name: , name:  , ]}, {name: , population: }]
+  
+  
+  
   const details = data[0].results.map(person => {
     return {
       name: person.name,
