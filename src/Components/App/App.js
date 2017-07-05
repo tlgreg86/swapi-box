031b936987d17018602b92d5fc4963c9f0fb9162
Travis <tlgreg86@gmail.com>
2017-07-04 22:47:26 -0600

    Add scroll API call, random number generator, and scroll styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,10 +5,30 @@ import CardList from '../CardList/CardList';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      scroll: []
+    }
+  }
+
+  scrollCall() {
+    const randomNum = Math.floor(Math.random() * (8 - 1)) + 1;
+    fetch(`http://swapi.co/api/films/${randomNum}`)
+      .then( response => response.json() )
+      .then( scroll => {
+        this.setState({scroll: scroll})
+      })
+  }
+
+  componentWillMount() {
+    this.scrollCall()
+  }
+
   render() {
     return (
       <div className='App'>
-        <Scroll />
+        <Scroll scroll={this.state.scroll}/>
         <div className='list-container'>
           <ButtonList />
           <CardList />
