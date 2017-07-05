6185bc597defb380eee01b60e6f7e9597bed61f0
Travis <tlgreg86@gmail.com>
2017-07-05 15:36:11 -0600

    Add helper file and begin set up of promise.all with people data.

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -2,19 +2,24 @@ import React from 'react';
 import Card from '../Card/Card'
 import './CardList.css';
 
-const CardList = () => {
+const CardList = ({ peopleData }) => {
+  console.log(peopleData);
   return (
     <div className='card-list'>
-      <Card name={'Luke Skywalker'}
-            homeworld={'Tattoine'}
-            species={'Human'}
-            population={'150000'}/>
+      {peopleData.map((person, index) =>
+        <Card key={index}
+              name={person.name}
+              homeworld={person.homeworld}
+              species={person.species}
+              population={person.homeworld}/>
+      )}
+
       <Card name={'Tattoine'}
             terrain={'rough'}
             population={'150000'}
             climate={'dry'}
             residents={'Luke Skywalker'}/>
-      <Card name={'AT-AT'} 
+      <Card name={'AT-AT'}
             model={'F-150'}
             class={'pickup'}
             numPassengers={'8'}/>
