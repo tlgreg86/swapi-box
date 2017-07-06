1ab1207f85dbffb90694df9ea268cf7c7e621a6b
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-05 19:23:42 -0600

    Start of tests

--- src/Components/CardList/CardList.test.js
+++ src/Components/CardList/CardList.test.js
@@ -3,7 +3,13 @@ import { shallow, mount } from 'enzyme';
 import CardList from './CardList';
 
 describe('CardList tests', () => {
-  // it('should render a div with className "card-list"')
+  it('should render a div with className "card-list"', () => {
+    let component = shallow(<CardList />);
+    const cardList = component.find('.card-list');
+    expect(cardList).toHaveLength(1);
+    // so I remember...
+    expect(cardList).toHaveLength(2);
+  });
 
   // it('should render a card for each Person in state')
   
