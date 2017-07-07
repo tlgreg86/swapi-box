ee23da2d382e5e3f501f2b18a32dc7d8c9f6c492
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-06 18:03:04 -0600

    Add Favorites functionality

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -2,22 +2,24 @@ import React from 'react';
 import Card from '../Card/Card'
 import './CardList.css';
 
-const CardList = ({ data, selectedTab }) => {
+const CardList = ({ data, selectedTab, handleFavorites }) => {
   return (
     <div className={'card-list ' + selectedTab.toLowerCase()}>
       {data.map((article, index) =>
-        <Card key={index}
-              name={article.name}
-              homeworld={article.homeworld}
-              species={article.species}
-              population={article.population}
-              terrain={article.terrain}
-              climate={article.climate}
-              residents={article.residents}
-              model={article.model}
-              vehicleClass={article.vehicleClass}
-              numPassengers={article.numPassengers}
-              />
+        // <Card key={index}
+        //       name={article.name}
+        //       homeworld={article.homeworld}
+        //       species={article.species}
+        //       population={article.population}
+        //       terrain={article.terrain}
+        //       climate={article.climate}
+        //       residents={article.residents}
+        //       model={article.model}
+        //       vehicleClass={article.vehicleClass}
+        //       numPassengers={article.numPassengers}
+        //       handleFavorites={handleFavorites}
+        //       />
+        <Card key={index} article={article} handleFavorites={handleFavorites}/>
       )}
     </div>
   );
