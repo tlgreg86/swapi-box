a48cd76a3d185aa78f8304fa686d834dbdef4770
Lindsay Parker <lindsay.w.parker@gmail.com>
2017-07-08 17:06:47 -0600

    Add Vehicles test and start of Planets test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -10,14 +10,36 @@ import { mockPerson,
 import App from './App';
 
 describe('App tests', () => {
-  const wrapper = shallow(<App />);
-  const resolveAfter2Seconds = () => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, 2000);
-    });
-  }
+  let wrapper;
+  let wrapperMounted;
+  let resolveAfter2Seconds;
+  
+  beforeEach( () => {
+    fetchMock.get('http://swapi.co/api/planets/1/', {
+      status: 200, 
+      body: mockPlanet
+    }).catch();
+    
+    fetchMock.get('http://swapi.co/api/vehicles/', {
+      status: 200, 
+      body: mockVehicle
+    }).catch();
+    
+    fetchMock.get('begin:http://swapi.co/api/films/', {
+      status: 200, 
+      body: mockScroll
+    }).catch();
+    
+    wrapper = shallow(<App />);
+    wrapperMounted = mount(<App />);
+    resolveAfter2Seconds = () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      });
+    }
+  })
   
   afterEach( () => {
     // expect(fetchMock.calls().unmatched).toEqual([]);
@@ -43,16 +65,36 @@ describe('App tests', () => {
   });
   
   it('scrollCall() should return an object containing a random scroll', async () => {
-    fetchMock.get('begin:http://swapi.co/api/films/', {
-      status: 200, 
-      body: mockScroll
-    })
-    .catch();
-    
-    const wrapperMounted = mount(<App />);
     await resolveAfter2Seconds();
     
     expect(fetchMock.called()).toEqual(true);
     expect(wrapperMounted.state().scroll).toEqual(mockScroll);
+  });
+  
+  it('fetchPlanets() should return an object containing planet data', async () => {
+    await resolveAfter2Seconds();
+    
+    expect(fetchMock.called()).toEqual(true);
+    expect(wrapperMounted.state().planets).toEqual([{
+      "name": "Tatooine", 
+      "climate": "arid", 
+      "terrain": "desert", 
+      "population": "200000", 
+      "residents": [
+        "http://swapi.co/api/people/" 
+      ]
+    }]);
+  });
+  
+  it('fetchVehicles() should return an object containing vehicle data', async () => {
+    await resolveAfter2Seconds();
+    
+    expect(fetchMock.called()).toEqual(true);
+    expect(wrapperMounted.state().vehicles).toEqual([{
+      "name": "Sand Crawler", 
+      "model": "Digger Crawler", 
+      "numPassengers": "30", 
+      "vehicleClass": "wheeled" 
+    }]);
   });
 });
