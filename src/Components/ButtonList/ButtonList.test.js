3e96afe6cf80bc437e0ccc030094b06efd26747d
Travis <tlgreg86@gmail.com>
2017-07-08 20:55:40 -0600

    Add tests checking that list renders, and 4 buttons render.

--- src/Components/ButtonList/ButtonList.test.js
+++ src/Components/ButtonList/ButtonList.test.js
@@ -3,10 +3,31 @@ import { shallow, mount } from 'enzyme';
 import ButtonList from './ButtonList';
 
 describe('ButtonList tests', () => {
-  // it('should render four buttons')
-  // it('should render a Favorites button')
+
+  const wrapper = shallow(<ButtonList />)
+
+  it('should render button list', () => {
+    expect(wrapper.find('.button-list')).toHaveLength(1)
+  })
+
+  it('should render four buttons', () => {
+    const mockedFn = jest.fn();
+    const wrapper = mount(<ButtonList selectedTab={''}
+                                      handleClick={mockedFn} />)
+
+    expect(wrapper.find('button')).toHaveLength(4)
+  })
+
+  // it('should render a Favorites button', () => {
+  //   const wrapper = mount(<ButtonList selectedTab={''} />);
+  //   console.log(wrapper.debug());
+  //
+  //   expect(wrapper.find('button').children().text()).toEqual('FAVORITES')
+  // })
+
   // it('should render a People button')
+
   // it('should render a Planets button')
+
   // it('should render a Vehicles button')
-  
 });
