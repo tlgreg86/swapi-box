6b1d94e0be5643b5ee1951815e6bf2d94435f3d3
Travis <tlgreg86@gmail.com>
2017-07-08 22:14:38 -0600

    Add tests checking that scroll div renders and all of the scrolls pieces render as well.

--- src/Components/Scroll/Scroll.test.js
+++ src/Components/Scroll/Scroll.test.js
@@ -3,12 +3,35 @@ import { shallow, mount } from 'enzyme';
 import Scroll from './Scroll';
 
 describe('Scroll tests', () => {
-  // it('should render a div with className "scroll"')
-  
-  // it('should have an episode number')
-  
-  // it('should have an episode title')
-  
-  // it('should have an episode date')
+
+  const mockScroll =
+        {
+        "title": "A New Hope",
+        "episode_id": 4,
+        "opening_crawl": "A long time ago in a galaxy far, far away",
+        "release_date": "1977-05-25"
+        }
+
+  const wrapper = shallow(<Scroll scroll={mockScroll}/>)
+
+  it('should render a div with className scroll', () => {
+    expect(wrapper.find('.scroll')).toHaveLength(1)
+  })
+
+  it('should have an episode number', () => {
+    expect(wrapper.find('.episode-number')).toHaveLength(1)
+  })
+
+  it('should have an episode title', () => {
+    expect(wrapper.find('.episode-title')).toHaveLength(1)
+  })
+
+  it('should have an episode date', () => {
+    expect(wrapper.find('.scroll-date')).toHaveLength(1)
+  })
+
+  it('should have an opening scroll', () => {
+    expect(wrapper.find('.opening-scroll')).toHaveLength(1)
+  })
 
 });
