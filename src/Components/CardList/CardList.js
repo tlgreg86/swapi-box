8b1f75d6cca07a246c749c4b66bad9549cc47534
Travis <tlgreg86@gmail.com>
2017-07-10 06:44:20 -0600

    Fix css for header and scroll and add div with text for when there aren't any favorites.

--- src/Components/CardList/CardList.js
+++ src/Components/CardList/CardList.js
@@ -4,9 +4,14 @@ import Card from '../Card/Card'
 import './CardList.css';
 
 const CardList = ({ data, selectedTab, handleFavorites }) => {
+  console.log(selectedTab);
   let cardListContent;
+  let yodaTalk = selectedTab === 'FAVORITES' ?
+    <div className='yoda-talk'>No favorites, you have. Add some favorites, you must! Yes, hmmm.</div> :
+    <div></div>;
+
   if (data.length) {
-    cardListContent = 
+    cardListContent =
       <div className={'inner-card-container'}>
         {data.map( (article, index) =>
           <Card key={index}
@@ -28,6 +33,7 @@ const CardList = ({ data, selectedTab, handleFavorites }) => {
 
   return (
     <div className={`card-list ${selectedTab.toLowerCase()}`}>
+      { yodaTalk }
       { cardListContent }
     </div>
   );
